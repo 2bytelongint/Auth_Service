37f80eb607bfbfd64d0382f3f822cfa3ead493f2
Pratik Paul <pratik4paul5@gmail.com>
2025-02-08 11:20:06 +0530

    Added get user by id in repo level

--- src/repo/user-repo.js
+++ src/repo/user-repo.js
@@ -25,6 +25,18 @@ class UserRepository {
             throw error;
         }
     }
+
+    async getById(userId){
+        try {
+            const user = await User.findByPk(userId,{
+                attributes: ['email', 'id'],
+            });
+            return user;
+        } catch (error) {
+            console.log("Something went wrong in the getByID of user-repo level");
+            throw error;
+        }
+    }
 }
 
 module.exports = UserRepository
